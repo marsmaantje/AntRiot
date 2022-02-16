197ef53edb24d569a5d287fa13c48638294f6149
casper <caspe@DESKTOP-B3HHEBC>
2022-02-16 17:43:44 +0100

    Implemented single particle

---
+++ GXPEngine/Objects/Particle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+
+namespace Objects
+{
+    class Particle : AnimationSprite
+    {
+        float animationSpeed;
+        public Particle(string filename, int cols, int rows, int frameCount, float animationSpeed) : base(filename, cols, rows, frameCount, addCollider:false)
+        {
+            this.animationSpeed = animationSpeed;
+        }
+
+        public void Update()
+        {
+            Animate(animationSpeed * Time.deltaTime / 1000f);
+            if(currentFrame == frameCount -1)
+                LateDestroy();
+        }
+    }
+}
